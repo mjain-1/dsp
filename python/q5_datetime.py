750f03253fd77cadbfaa10efdb150d5490a82f9f
mjain-1 <meghaj67@gmail.com>
2016-08-27 16:52:55 -0400

    added responses

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,16 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+print ((date(2015, 7, 28) - date(2013, 1, 2)).days)
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+print ((date(2015, 5, 28) - date(2013, 12, 31)).days)
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+print ((date(2015, 7, 14) - date(1994, 1, 15)).days)
