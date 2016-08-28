476fa81f145067d6f09cdb281cf3b3987d8a6407
mjain-1 <meghaj67@gmail.com>
2016-08-28 19:51:44 -0400

    added all ex

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import re
 
-## Ex 1
 def extract_deg(deg):
     deg = str(deg)
     found = []
@@ -21,16 +20,51 @@ def extract_deg(deg):
         found.append('Sc.D.')
     return found
  
+def extract_pos(pos):
+    pos = str(pos)
+    found = []
+    if len(re.findall('Assist', pos)) >= 1:
+        found.append('Assistant Professor')
+    if len(re.findall('Associate', pos)) >= 1:
+        found.append('Associate Professor')
+    if len(re.findall('^Professor', pos)) >= 1:
+        found.append('Professor')
+    return found
+    
+def extract_domain(email):
+    email = str(email)
+    return re.findall('(?<=@)[a-z.]*', email)[0]
+    
 faculty = pd.read_csv('/Users/meghajain/Desktop/faculty.csv')
 
 all_deg = dict()
     
-for deg in faculty[" degree"]:
+for deg in faculty[' degree']:
     indiv_deg = extract_deg(deg)
     for degree in indiv_deg:
         if degree not in all_deg:
             all_deg[degree] = 1
         else:
             all_deg[degree] += 1
-            
-print("Degrees and their frequency: " all_deg)
+
+all_pos = dict()
+for pos in faculty[' title']:
+    indiv_pos = extract_pos(pos)
+    for position in indiv_pos:
+        if position not in all_pos:
+            all_pos[position] = 1
+        else:
+            all_pos[position] += 1
+
+emails = []
+domains = []
+for email in faculty[' email']:
+    emails.append(email)
+    domain = extract_domain(email)
+    if domain not in domains:
+        domains.append(domain)
+
+print("Degrees and their frequencies: ", all_deg)
+print("Positions and their frequencies: ", all_pos)     
+print("Emails: ", emails)
+print("Unique domains: ", domains)   
